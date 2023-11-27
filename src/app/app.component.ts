637fc2a850342224ad38c005351497f54e7bb323
Elyes Sellami <elyesellami@gmail.com>
2023-11-27 02:28:07 +0100

    added a formgroup for some reason

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection : ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'testing';
+  username: string = '';
+  password: string = '';
+  wasWelcomed = false;
+  theForm: FormGroup = new FormGroup({
+    theUser: new FormControl('', Validators.required),
+    thePassword: new FormControl('', Validators.required),
+  });
+
+  showWelcome() {
+    alert('Welcome !');
+    this.wasWelcomed = true;
+  }
 }
